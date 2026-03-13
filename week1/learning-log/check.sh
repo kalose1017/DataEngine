18a32927aa5c589dafcd990d8f1bde1df31ecb98
kalose1017 <leejiho1369@naver.com>
2026-03-13 14:43:47 +0000

    Refactor: Unified directory structure for Week 1 & 2

---
+++ week1/learning-log/check.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "---------------------------"
+echo "Study Compelete Day - March 13, 2026"
+echo "Study INFO: Linux and Git terminal command Basic"
+echo "---------------------------"
