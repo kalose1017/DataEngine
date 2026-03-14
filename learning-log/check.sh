759be3a1760c6083ef24882828c141799b11e917
kalose1017 <86546654+kalose1017@users.noreply.github.com>
2026-03-14 09:21:47 +0900

    File Refactoring

--- learning-log/check.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "---------------------------"
-echo "Study Compelete Day - March 13, 2026"
-echo "Study INFO: Linux and Git terminal command Basic"
-echo "---------------------------"
